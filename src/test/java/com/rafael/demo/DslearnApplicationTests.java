72df93d451b6e0103f6426ce4bf3331126e6280d
Rafael da Silva Marinheiro <rsmarinheiro007@gmail.com>
2025-02-02 21:26:24 -0300

    Create Project

---
+++ src/test/java/com/rafael/demo/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
